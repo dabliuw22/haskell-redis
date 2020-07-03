ba7210121dd175f32549bdcfa59b838178cc3d1e
Will Leyton <wleylop22@gmail.com>
2020-07-03 16:31:23 -0500

    Add MonadCatch

--- src/Adapter/Redis/Products.hs
+++ src/Adapter/Redis/Products.hs
@@ -6,11 +6,12 @@ import qualified Database.Redis as R
 import qualified Data.ByteString.Char8 as B
 import qualified Domain.Products as P
 import Data.Maybe (fromJust)
+import Control.Monad.Catch
 import Control.Monad.IO.Class
 
 fields = ["id", "name", "stock"]
 
-class (Functor m, Monad m) => ProductRepository m where
+class (Functor m, Monad m, MonadCatch m) => ProductRepository m where
   get :: R.Connection -> String -> m (Maybe P.Product)
   set :: R.Connection -> P.Product -> m ()
   del :: R.Connection -> String -> m ()
@@ -19,23 +20,22 @@ instance ProductRepository IO where
   get conn id = do
     result <- exec conn $ R.hmget (B.pack id) fields
     case result of
-          Right list -> case list of 
-                         []                          -> return Nothing
-                         [Nothing, Nothing, Nothing] -> return Nothing
-                         (h : t)                     -> toDomain (h:t)
-          Left e     -> return Nothing
+          Right list -> case list of
+                              (h : t) -> toDomain (h:t)
+                              []      -> return Nothing
+          Left e     -> throwM $ P.ProductException ("Not Found: " ++ show id)
   set conn product = do
     cmd <- fromDomain product
     let key = B.pack $ P.id (P.productId product)
     status <- exec conn $ R.hmset key cmd
     case status of
           Right R.Ok -> expire conn key >>= \s -> return ()
-          Left e     -> return ()
+          Left e     -> throwM $ P.ProductException "Not Create"
   del conn id = do
     result <- exec conn $ R.hdel (B.pack id) fields
     case result of
           Right a -> return ()
-          Left e  -> return ()
+          Left e  -> throwM $ P.ProductException "Not Delete"
 
 expire :: MonadIO m => R.Connection -> B.ByteString -> m Bool
 expire conn key = do
@@ -53,12 +53,15 @@ exec conn action = do
 
 toDomain :: Monad m => [Maybe B.ByteString] -> m (Maybe P.Product)
 toDomain list = do
-  let newList = map (B.unpack . fromJust) list
-  return $ Just $ P.Product {
-    P.productId = P.ProductId { P.id = head newList },
-    P.productName = P.ProductName { P.name = newList !! 1 },
-    P.productStock = P.ProductStock { P.stock = read (newList !! 2) :: Double }
-  }
+  case list of
+    [Nothing, Nothing, Nothing]   -> return Nothing
+    fields @ [id', name', stock'] -> let newList = map (B.unpack . fromJust) fields in
+      return $ Just $ P.Product {
+        P.productId = P.ProductId { P.id = head newList },
+        P.productName = P.ProductName { P.name = newList !! 1 },
+        P.productStock = P.ProductStock { P.stock = read (newList !! 2) :: Double }
+      }
+    _                             -> return Nothing
 
 fromDomain :: Monad m => P.Product -> m [(B.ByteString, B.ByteString)]
 fromDomain product = do
--- src/Domain/Products.hs
+++ src/Domain/Products.hs
@@ -1,5 +1,8 @@
 module Domain.Products where
 
+import Control.Exception
+import Data.Typeable
+
 newtype ProductId = ProductId { id :: String } deriving (Show, Eq)
 
 newtype ProductName = ProductName { name :: String } deriving (Show, Eq)
@@ -12,3 +15,7 @@ data Product =
     productName :: ProductName,
     productStock :: ProductStock
   } deriving (Show, Eq)
+
+data ProductException = ProductException String deriving Show
+
+instance Exception ProductException
